379f923025d16e2ffa23653b127db3c665ec330e
Dominique Oyco <dominickoyco@gmail.com>
2020-04-17 22:52:23 -0700

    implemented the EnumerateFilesRecursively method and main
    //TODO: implement FormatByteSize and CreateReport

--- fileReport.cs
+++ fileReport.cs
@@ -1,9 +1,56 @@
-using System;
+using System;                       //contains fundamental classes and base classes 
+using System.IO;                    //allows us to read and write files in C#
+using System.Linq;                  //provides classes and interfaces that support queries that use Language-Integrated Query (LINQ)
+using System.Xml.Linq;              //needed to use linq's xml builder methods
+using System.Collections.Generic;   //needed to access C#'s data structures
 
+/*
+This program uses C# and LINQ to iterate files, to query, group, and order data, 
+and to create an XML document based on that data
+*/
 namespace labAssignment4
 {
     class fileReport
     {
+        /*
+        This function enumerates all files in a given folder recursively including 
+        the entire sub-folder hierarchy
+        */
+        static IEnumerable<string> EnumerateFilesRecursively(string path)
+        {
+            foreach (string d in Directory.EnumerateDirectories(path))          //iterate through the folder
+            {
+                foreach (string f in Directory.EnumerateFiles(d))               //iterate through the files in the folder
+                {
+                    yield return f;                                             //generate the iterator
+                }
+                EnumerateFilesRecursively(d);
+            }            
+        }
+
+        /*
+        This function formats a byte size in human readable form
+        */
+        static string FormatByteSize(long byteSize){
+            //TODO: Implement this function
+        }
+
+        /*
+        Creates the necessary html report file containing a table with three 
+        columns: "Type", "Count", & "Size" for the file name extension, the 
+        number of files with this type, and the total size of all files with 
+        the type respectively
+        */
+        static XDocument CreateReport(IEnumerable<string> files)
+        { 
+            //TODO: Implement this function
+        }
+
+        /*
+        The main function takes in two command line arguments: 
+        1.) The path to the input folder
+        2.) The path of the HTML report output file
+        */
         static void Main(string[] args)
         {
             try
@@ -11,6 +58,7 @@ namespace labAssignment4
                 //command line args for folder location and file output
                 string folder = args[0];
                 string reportfile = args[1];
+                //TODO: call CreateReport() and pass in the folder the file output as parameters
             }
             catch
             {
