dd3598a31a890e5a6680f1c070834fad5362ec49
Dominique Oyco <dominickoyco@gmail.com>
2020-04-20 14:02:15 -0700

    fileReport final??? version

--- lab4tester424/FileReport.cs
+++ lab4tester424/FileReport.cs
@@ -4,7 +4,7 @@
 using System;                       //contains fundamental classes and base classes 
 using System.IO;                    //allows us to work with files and directories in C#
 using System.Linq;                  //provides classes and interfaces that support queries that use Language-Integrated Query (LINQ)
-using System.Xml.Linq;              //needed to use linq's xml builder methods
+using System.Xml.Linq;              //needed to use LINQ's xml builder methods
 using System.Collections.Generic;   //needed to access C#'s data structures
 
 //TERMINAL COMMAND TO RUN: dotnet run /Users/rain/Desktop/lol /Users/rain/Desktop/lol/lol.html
@@ -18,8 +18,10 @@ namespace lab4tester424
     internal static class FileReport
     {
         /*
-        This function enumerates all files in a given folder recursively including 
+        This recursive function enumerates all files in a given folder recursively including 
         the entire sub-folder hierarchy
+        Param: path (string) - the input directory that the user has provided
+        Return: file (string) - an iterator that will iterate through the directory when files are found
         */
         static IEnumerable<string> EnumerateFilesRecursively(string path) 
         {
@@ -34,29 +36,41 @@ namespace lab4tester424
 
 
         /*
-        All the byte size units a file can have. Similar to the preview shown in lab.
+        All the byte size units a file can have. Inspired by the preview shown in lab.
         */
-        private enum FileSizes : byte { B, KB, MB, GB, TB, PB, EB, ZB, YB }                 //units (bytes) for the sizes of the file
+        private enum FileSizes : byte { B, KB, MB, GB, TB, PB, EB, ZB}   //units (bytes) for the sizes of the file
 
         /*
         This function formats a byte size in human readable form
+        Param: byteSize (long) - byte size of a file 
+        Return: byte size of a file formatted to two fixed decimal digits and it's corresponding byte unit (string)
         */
         static string FormatByteSize(long byteSize)
         {
             double formattedByteSize = byteSize;
-            const double kiloValue = 1000;
+            const double k = 1000;
 
             FileSizes fileSize = 0;
-            //
-            for(fileSize = FileSizes.B; formattedByteSize >= kiloValue && fileSize < FileSizes.YB; ++fileSize)
+            //Divide the byte size by 1000 if the file size is >= 1000 and if it's less than the maximum byte unit (ZB)
+            //to make sure that the numerical sizes of files are > 1 and < 1000
+            for(fileSize = FileSizes.B; formattedByteSize >= k && fileSize < FileSizes.ZB; ++fileSize)
             {
-                formattedByteSize /= kiloValue;
+                formattedByteSize /= k;        //
             }
             
-            return(formattedByteSize.ToString("N2") + " " + fileSize.ToString());    //N2 is equivalent to %.2f in other languages. It prints a double/float in fixed 2 decimal numbers
+            //N2 is equivalent to %.2f in other languages. It prints a double/float in fixed 2 decimal numbers
+            return(formattedByteSize.ToString("N2") + " " + fileSize.ToString());    
         }
 
-        public static XElement GetFileInfo(IEnumerable<string> files)
+        /*
+        This function constructs the table that will go inside the html report. 
+        It uses the input directory from the main to look for the information about the files i.e. file type (.txt, .jpg,
+        etc.), number of files of the same type, and the file size. The data is eventually displayed on the table.
+        Param: files (IEnumerable<string>) - a string object used to traverse through the folder to looks for files (iterator)
+        Return: table (XElement) - the constructed table that displays the data of a directory 
+                and it's corresponding sub-directory
+         */
+        static XElement GetFileInfo(IEnumerable<string> files)
         {
             XElement table = new XElement("table",
                 new XAttribute("style", "width : 50%"),
@@ -84,19 +98,18 @@ namespace lab4tester424
         }
         
         /*
-        Creates the necessary html report file containing a table with three 
-        columns: "Type", "Count", & "Size" for the file name extension, the 
-        number of files with this type, and the total size of all files with 
-        the type respectively
+        Constructs the html file by building the head, title, style, and body of the html webpage
+        Param: files (IEnumerable<string>) - a string object used to traverse through the folder to looks for files (iterator)
+        Return: report (XDocument) - the html webpage containing the data from a directory that the user provided
         */
-        private static XDocument CreateReport(IEnumerable<string> files)
+        static XDocument CreateReport(IEnumerable<string> files)
         {
             XDocument report = new XDocument(new XElement("html",
                     new XElement("head",
                         new XElement("title", "File Report"),
                         new XElement("style", "table, th, td {border: 2px solid black;}"),
                         new XElement("body",
-                            new XElement("td", GetFileInfo(files))
+                            new XElement("td", GetFileInfo(files))    //call the method above to construct the table
                         )
                     )
             ));
@@ -107,6 +120,8 @@ namespace lab4tester424
         The main function takes in two command line arguments: 
         1.) The path to the input folder
         2.) The path of the HTML report output file
+        
+        catch if there is no directory or if it's invalid
         */
         static void Main(string[] args)
         {
@@ -117,9 +132,9 @@ namespace lab4tester424
                 
                 CreateReport(EnumerateFilesRecursively(inputFolder)).Save(outputFile);  //searches for the 
             }
-            catch
+            catch //If an input or output directory does not exist, print the message below
             {
-                Console.WriteLine("there's nothing...");                
+                Console.WriteLine("there's nothing...");            
             }
         }
     }
